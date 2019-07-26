3292176ff5ddc6972d7925c3054ea31e7e997196
Wellington Garcia <wellington.garcia@mirumagency.com>
2019-07-26 09:34:28 -0300

    Rota default

--- src/routes.js
+++ src/routes.js
@@ -6,5 +6,8 @@ const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+routes.get('/', (req, res) => {
+  return res.json({ error: 'password error2' });
+});
 
 export default routes;

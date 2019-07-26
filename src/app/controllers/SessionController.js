db8c6d6b83ce12085230d4d59018cac8dc1b7d15
Wellington Garcia <garciawell@gmail.com>
2019-07-26 00:36:39 -0300

    Controle de acesso JWT

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'password error' });
+    }
+
+    const { id, name } = user;
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, 'bbb5ff6dc3826b999a5cf0c2e7b2c889', {
+        expiresIn: '7d',
+      }),
+    });
+  }
+}
+
+export default new SessionController();

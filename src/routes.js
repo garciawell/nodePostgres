6608ea6231f8233d5289d859782abfa207c2f010
Wellington Garcia <wellington.garcia@mirumagency.com>
2019-07-30 13:24:42 -0300

    Middleware autenticacao

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-routes.get('/', (req, res) => {
-  return res.json({ error: 'password error2' });
-});
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
 
 export default routes;

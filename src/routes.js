f1878058cb6c0360b0b3dabbabefc76e0a6e235b
Wellington Garcia <garciawell@gmail.com>
2019-08-04 02:08:04 -0300

    Controller e rota de Notificação lida

--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,7 @@ routes.get('/appointments', AppointmentsController.index);
 
 routes.get('/schedule', ScheduleController.index);
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 

8ff54927c1e4a4e6f73bd7146d9b56bc731cc3b9
Wellington Garcia <garciawell@gmail.com>
2019-08-03 23:10:43 -0300

    Listando data para provider somente do dia

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import ProviderController from './app/controllers/ProviderController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 import AppointmentsController from './app/controllers/AppointmentsController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,9 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentsController.store);
 routes.get('/appointments', AppointmentsController.index);
+
+routes.get('/schedule', ScheduleController.index);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;

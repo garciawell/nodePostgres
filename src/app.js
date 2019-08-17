0084d9c694901219e12f3aa9ba8698b711c0bb6b
Wellington Garcia <garciawell@gmail.com>
2019-08-16 23:26:14 -0300

    Adicionando Exceções e  monitoramento em prod

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
-
+import sentryConfig from './config/Sentry';
+import 'express-async-errors';
 import './database';
 
 class App {
   constructor() {
     this.server = express();
-
+    Sentry.init(sentryConfig);
     this.middlewares();
     this.routes();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -22,6 +25,7 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
   }
 }
 

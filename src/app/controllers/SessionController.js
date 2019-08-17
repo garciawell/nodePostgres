03588af66326d7c07b7ec992e17c565ce5b97cc6
Wellington Garcia <garciawell@gmail.com>
2019-08-17 20:47:48 -0300

    Alterações para Front-End

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,11 +2,22 @@ import jwt from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../../config/auth';
 
+import File from '../models/File';
+
 class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
       return res.status(401).json({ error: 'User not found' });
@@ -16,12 +27,14 @@ class SessionController {
       return res.status(401).json({ error: 'password error' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,4 +1,5 @@
 import User from '../models/User';
+import File from '../models/File';
 
 import Cache from '../../lib/Cache';
 
@@ -39,9 +40,17 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
-
-    return res.json({ id, name, email, provider });
+    // const { id, name, provider } = await user.update(req.body);
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+    return res.json({ id, name, email, avatar });
   }
 }
 

24ac0cce21454739cbf82b147b0442318bd29146
Wellington Garcia <garciawell@gmail.com>
2019-08-17 12:48:00 -0300

    Security: Cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import helmet from 'helmet';
 import redis from 'redis';
 import RateLimit from 'express-rate-limit';
@@ -24,6 +25,7 @@ class App {
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(helmet());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
